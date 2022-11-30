4c64263e3eb4ced0c5c3811758d958600940c28b
Hemant Kumar Sahu <hemantjava90@gmail.com>
2022-11-30 08:01:47 +0530

    Delete src directory

--- src/main/java/com/hemant/exception/EmpNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hemant.exception;
-
-public class EmpNotFoundException extends RuntimeException {
-    public EmpNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/test/java/com/hemant/data/MockData.java
+++
@@ -1,6 +0,0 @@
-package com.hemant.data;
-
-public class MockData {
-
-
-}
