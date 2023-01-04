da374fa7aec647e9a0d090bdcd412ddbf0830e98
Hemant Kumar Sahu <hemantjava90@gmail.com>
2023-01-04 12:48:24 +0530

    Add files via upload

---
+++ src/main/java/com/hemant/model/dto/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.hemant.model.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ErrorResponse {
+    private Integer status;
+    private String errorMessage;
+    private LocalDateTime dateTime;
+}
